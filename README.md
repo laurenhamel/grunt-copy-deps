f1cdfbc071c456f9e3c1271b6c30003b27dd8335
Lauren Hamel <lauren.hamel@emory.edu>
2017-11-30 16:04:41 -0500

    Adds in support for CSS files, includes, and excludes
    Adds in support for copying CSS files as well as including additional paths to be copied, which can be useful for supplemental files. Also allows for exclusions, or paths that should be excluded from copying.

--- README.md
+++ README.md
@@ -42,37 +42,216 @@ Default: `false`
 Copy the unminified version of each dependency.
 
 
-#### exclude
+#### ignore
 
 Type: `Array`  
 Default: `[]`
 
-An array containing the name of dependencies that should be excluded from copying.
+An array containing the name of the dependencies (as listed in your package file) that should be ignored.
 
+#### exclude
+
+Type: `Object`
+Default: 
+```
+{
+  js: [],
+  css: []
+}
+```
+
+An object identifying the `.js` and/or `.css` files that should not be copied.
+
+
+#### include
+
+Type: `Object`
+Default: 
+```
+{
+  js: {},
+  css: {}
+}
+```
+
+An object identifying the `.js` and/or `.css` files that should also be copied along with other dependency files.
+
+
+#### js
+
+Type: `Boolean`
+Default: `true`
+
+Determines whether or not `.js` files for all dependencies should be copied.
+
+
+#### css
+
+Type: `Boolean`
+Default: `false`
+
+Determines whether or not `.css` files for all dependencies should be copied.
 
 
 ### Examples
 
 Add a section named `copydeps` to the data object passed into `grunt.initConfig()`.
 
+**Copying only `.min.js` dependencies:**
+
+```js
+grunt.initConfig({
+  copydeps: {            
+    target: {            
+      pkg: 'package.json',
+      dest: 'dest/js/dependencies/'
+    }
+  }
+});
+```
+
+**Copying only `.js` dependencies:**
+
 ```js
 grunt.initConfig({
   copydeps: {            
     target: {            
       options: {   
-        minified: true,
-        unminified: false,
-        exclude: []
+        minified: false,
+        unminified: true
       },
       pkg: 'package.json',
       dest: 'dest/js/dependencies/'
     }
   }
 });
-
-grunt.loadNpmTasks('grunt-copy-deps');
-
-grunt.registerTask('default', ['copydeps']);
 ```
 
-The `copydeps` task will look for `dependencies` listed in your `package.json` file, and dependencies will be copied into your `dest` folder.
+**Copying both `.min.js` and `.js` dependencies:**
+
+```js
+grunt.initConfig({
+  copydeps: {            
+    target: {            
+      options: {   
+        unminified: true,
+      },
+      pkg: 'package.json',
+      dest: 'dest/js/dependencies/'
+    }
+  }
+});
+```
+
+**Copying both `.js` and `.css` dependencies:**
+
+```js
+grunt.initConfig({
+  copydeps: {            
+    target: {            
+      options: {  
+        minified: true,
+        unminified: true,
+        css: true,
+      },
+      pkg: 'package.json',
+      /**
+       * Use an Object with keys `css` and `js`
+       * if you wish to place dependencies
+       * in different locations based on the
+       * type of file.
+       */
+      dest: {
+        css: 'dest/css/dependencies/',
+        js: 'dest/js/dependencies/'
+      }
+    }
+  }
+});
+```
+
+
+**Copying only `.css` dependencies:**
+
+```js
+grunt.initConfig({
+  copydeps: {            
+    target: {            
+      options: {  
+        minified: true
+        unminified: true,
+        css: true,
+        js: false
+      },
+      pkg: 'package.json',
+      dest: 'dest/css/dependencies/'
+    }
+  }
+});
+```
+
+**Copying with some dependencies ignored:**
+
+```js
+grunt.initConfig({
+  copydeps: {            
+    target: {            
+      options: {   
+        ignore: [ 'jquery' ]
+      },
+      pkg: 'package.json',
+      dest: 'dest/js/dependencies/'
+    }
+  }
+});
+```
+
+**Copying with excluded files:**
+
+```js
+grunt.initConfig({
+  copydeps: {            
+    target: {            
+      options: {   
+        /**
+         * All files paths are relative to
+         * the `node_modules/` directory.
+         */
+        exclude: {
+          js: ['jquery/src/**/*.js']
+      },
+      pkg: 'package.json',
+      dest: 'dest/js/dependencies/'
+    }
+  }
+});
+```
+  
+**Copying with included files:**
+
+```js
+grunt.initConfig({
+  copydeps: {            
+    target: {            
+      options: {   
+        include: {
+          /**
+           * Object syntax resembles { <src>: <dest> }, 
+           * where <dest> refers to a path relative to 
+           * your `dest/` directory. You can use
+           * '.' if no subfolder is desired. All <src>
+           * file paths are relative to the `node_moduels/`
+           * directory.
+           */
+          js: {
+            'codemirror/mode/**/*.js': 'codemirror/'
+          }
+      },
+      pkg: 'package.json',
+      dest: 'dest/js/dependencies/'
+    }
+  }
+});
+```
+
+The `copydeps` task will look for `dependencies` listed in your package file, and these dependencies will be copied into your `dest` folder.
--- tasks/copydeps.js
+++ tasks/copydeps.js
@@ -18,36 +18,136 @@ module.exports = function (grunt) {
     var options = this.options({
       minified: true,
       unminified: false,
-      exclude: []
+      js: true,
+      css: false,
+      ignore: [],
+      exclude: {
+        css: [],
+        js: []
+      },
+      include: {
+        css: {},
+        js: {}
+      }
     });
     
     // Read package data.
     var pkg = grunt.file.readJSON(path.resolve(this.data.pkg));
     
     // Get the target destination.
-    var dest = this.data.dest;
+    var dest = this.data.dest; 
     
     // Get dependencies.
     var dependencies = Object.keys(pkg.dependencies);
     
     // Create globs of the dependencies.
-    var globs = [];
+    var files = [];
     
     dependencies.forEach(function(dependency){
       
       // Ignore exclusions.
-      if( options.exclude.indexOf(dependency) > -1 ) return;
+      if( options.ignore.indexOf(dependency) > -1 ) return;
       
-      // Attempt to use 
+      // Start a glob path.
       var glob = 'node_modules/' + dependency + '/**/' + dependency;
       
-      if( options.minified === true ) globs.push( glob + '.min.js' );
-      if( options.unminified === true ) globs.push( glob + '.js' );
+      if( options.js === true ) {
+        
+        if( options.minified === true ) files.push( glob + '.min.js' );
+        if( options.unminified === true ) files.push( glob + '.js' );
+        
+      }
+      
+      if( options.css === true ) {
+        
+        if( options.minified === true ) files.push( glob + '.min.css' );
+        if( options.unminified === true ) files.push( glob + '.css' );
+        
+      }
+      
+    });
+    
+    // Create globs of excluded files.
+    var excludes = [];
+  
+    if( options.exclude.css ) {
+      
+      options.exclude.css.forEach(function(css){
+      
+        // Start a glob path.
+        var glob = 'node_modules/';
+
+        excludes.push( glob + css );
+
+      });
+      
+    }
+    if( options.exclude.js ) {
+      
+      options.exclude.js.forEach(function(js){
+      
+      // Start a glob path.
+      var glob = 'node_modules/';
+      
+      excludes.push( glob + js );
+      
+    });
+      
+    }
+    
+    // Create globs of included files.
+    var includes = [];
+    
+    if( options.include.css ) {
+      
+      // Start a glob path.
+      var glob = 'node_modules/';
+      
+      for(var css in options.include.css) {
+        
+        includes.push({ src: glob + css, dest: options.include.css[css] });
+        
+      }
+      
+    }
+    if( options.include.js ) {
+      
+      // Start a glob path.
+      var glob = 'node_modules/';
+      
+      for(var js in options.include.js) {
+        
+        includes.push({ src: glob + js, dest: options.include.js[js] });
+        
+      }
+      
+    }
+
+    // Resolve globs.
+    files = grunt.file.expand(files);
+    excludes = grunt.file.expand(excludes);
+    includes = includes.map(function(include){
+      
+      include.src = grunt.file.expand(include.src);
+      
+      return include;
       
     });
   
-    // Resolve globs.
-    var files = grunt.file.expand(globs);
+    // Remove excluded files.
+    files = files.filter(function(file){
+      
+      var filename = path.basename(file),
+          filenames = excludes.map(function(exclude){
+            return path.basename(exclude);
+          });
+      
+      if( excludes.indexOf(file) > -1 ) return false; 
+      if( filenames.indexOf(filename) > -1 ) return false;
+      
+      return true;
+      
+    });
     
     // Set directory precedence for dependencies.
     var precedence = [
@@ -93,24 +193,94 @@ module.exports = function (grunt) {
           unminified = ordered.filter(function(file){
             return minified.indexOf(file) === -1;
           });
+      
+      // Extract CSS and JS.
+      if( minified.length > 0 ) {
+        
+        minified.js = minified.filter(function(file){
+        return file.indexOf('.js') > -1;
+        });
+        minified.css = minified.filter(function(file){
+        return file.indexOf('.css') > -1;
+      });
+        
+      }
+      if( unminified.length > 0 ) {
+      
+        unminified.js = unminified.filter(function(file){
+        return file.indexOf('.js') > -1;
+      
+      });
+        unminified.css = unminified.filter(function(file){
+        return file.indexOf('.css') > -1;
+      });
+        
+      }
 
       // Initialize a set of files to keep.
       var keep = [];
       
       // Get first index of minified/unminified files.
-      if( options.minified === true && minified.length > 0 ) keep.push( minified[0] );
-      if( options.unminified === true && unminified.length > 0 ) keep.push( unminified[0] );
-      
+      if( options.minified === true && minified.length > 0 ) {
+
+        keep.push( minified.js[0] );
+        
+        if( options.css === true && minified.css.length > 0 ) keep.push( minified.css[0] );
+        
+      }
+      if( options.unminified === true && unminified.length > 0 ) {
+        
+        if( options.js === true && unminified.js.length > 0 ) keep.push( unminified.js[0] );
+        
+        if( options.css === true && unminified.css.length > 0 ) keep.push( unminified.css[0] );
+        
+      }
+ 
       // Fetch and copy dependencies.
       keep.forEach(function(file){
         
         // Change the destination.
-        var filename = path.basename(file);
+        var filename = path.basename(file), destination;
         
-        grunt.file.copy(file, path.resolve(dest, filename));
+        if( options.css === true && file.indexOf('.css') > -1 ) {
+            
+          destination = dest instanceof Object ? dest.css : dest;
+          
+        }
+        
+        if( options.js === true && file.indexOf('.js') > -1 ) {
+            
+          destination = dest instanceof Object ? dest.js : dest;
+          
+        }
+        
+        grunt.file.copy(file, path.resolve(destination, filename));
         
       });
       
+    });
+    
+    // Also fetch included files.
+    includes.forEach(function(include){
+      include.src.forEach(function(file){
+        
+        var filename = path.basename(file), destination = [null, include.dest];
+
+        if( options.css === true && file.indexOf('.css') > -1 ) {
+
+          destination[0] = dest instanceof Object ? dest.css : dest;
+
+        }
+
+        if( options.js === true && file.indexOf('.js') > -1 ) {
+
+          destination[0] = dest instanceof Object ? dest.js : dest;
+
+        }
+
+        grunt.file.copy(file, path.resolve(path.resolve.apply(null, destination), filename));
+        
+      });
     });
     
   });
